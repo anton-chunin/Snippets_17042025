96477e15b64c1cfdb68b76f8eeb49d755bc8693b
Anton Chunin <antonc89@gmail.com>
2025-04-20 17:54:53 +0300

    django form in process...

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,9 +1,15 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from MainApp.models import Snippet
 
 
 class SnippetForm(ModelForm):
-   class Meta:
-       model = Snippet
-       # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code']
+    class Meta:
+        model = Snippet
+        # Описываем поля, которые будем заполнять в форме
+        fields = ['name', 'lang', 'code']
+
+    def clean_name(self):
+        snippet_name = self.cleaned_data.get("name")
+        if len(snippet_name) > 3:
+            return snippet_name
+        raise ValidationError("Snippet name too short")
