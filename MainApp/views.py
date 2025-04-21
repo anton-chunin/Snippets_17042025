deaf3bec1794d49a7a2d1589084c9d2e33e9c764
Anton Chunin <antonc89@gmail.com>
2025-04-21 15:50:19 +0300

    Done. Edit snippet

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
 from MainApp.forms import SnippetForm
@@ -44,13 +44,42 @@ def snippet_detail(request, snippet_id: int):
         return render(request, "pages/errors.html", context | {"error": f"Snippet with id={snippet_id} not found"})
     else:
         context["snippet"] = snippet
+        context["type"] = "view"
         return render(request, "pages/snippet_detail.html", context)
     
 
-# def create_snippet(request):
-#     if request.method == "POST":
-#         form = SnippetForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("snippets-list")
-#         return render(request, "pages/add_snippet.html", {'form': form})
+def snippet_edit(request, snippet_id: int):
+    context = {"pagename": "Редактирование сниппета"}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return Http404
+    #Variant 1
+    #======= Получение данных сниппета с помощью SnippetForm
+    # if request.method == "GET":
+    #     form = SnippetForm(instance=snippet)
+    #     return render(request, 'pages/add_snippet.html', {"form": form})
+    #===================================================================
+
+    #Variant 2
+    #хотим получить страницу с данными сниппета
+    if request.method == "GET":
+        context = {
+            'snippet': snippet,
+            "type": "edit"
+            }
+        return render(request, 'pages/snippet_detail.html', context)
+    #получаем данные из формы и на их основе создаем новый сниппет в БД
+    if request.method == "POST":
+        data_form = request.POST
+        snippet.name = data_form["name"]
+        snippet.code = data_form["code"]
+        snippet.save()
+        return redirect("snippets-list")# GET /snippets/list
+
+
+def snippet_delete(request, snippet_id: int):
+    if request.method == "POST" or request.method == "GET":
+        snippet = get_object_or_404(Snippet, id=snippet_id)
+        snippet.delete()
+    return redirect("snippets-list")
