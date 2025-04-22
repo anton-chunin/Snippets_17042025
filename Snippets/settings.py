3ddebe1faa827a114076af181f22bba77b435ee6
Anton Chunin <antonc89@gmail.com>
2025-04-22 19:48:39 +0300

    Edit hosts

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'antonchunin.pythonanywhere.com']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
